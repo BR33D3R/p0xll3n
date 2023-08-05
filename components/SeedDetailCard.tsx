1625587bfc5e148b3767c81f4d31a9764a4cbb1c
BR33D3R <141458032+BR33D3R@users.noreply.github.com>
2023-08-04 20:24:36 -0700

    Update SeedDetailCard.tsx
    psuedo code/ todos

---
+++ components/SeedDetailCard.tsx
@@ -0,0 +1,13 @@
+// Define component
+SeedDetail(props) {
+  // Initialize state for seed details
+  state = {seed: null}
+
+  // When component mounts or when props.seedId changes, fetch seed details from blockchain and update state
+
+  // Render seed details
+  render() {
+    // If seed data is not yet loaded, display loading message
+    // Else, display seed details based on state
+  }
+}
