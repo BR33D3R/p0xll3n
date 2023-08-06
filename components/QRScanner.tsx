206d637a20a47217545c1b8aa868ebcbea9eb39a
BR33D3R <141458032+BR33D3R@users.noreply.github.com>
2023-08-06 03:00:54 -0700

    Update QRScanner.tsx
    npm install react-qr-reader

---
+++ components/QRScanner.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import QrReader from 'react-qr-reader';
+
+class TestScanner extends Component {
+  state = {
+    result: 'No result'
+  }
+
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+      // Now data contains the scanned information, you can handle it here
+    }
+  }
+  
+  handleError = err => {
+    console.error(err)
+  }
+
+  render() {
+    return (
+      <div>
+        <QrReader
+          delay={300}
+          onError={this.handleError}
+          onScan={this.handleScan}
+          style={{ width: '100%' }}
+        />
+        <p>{this.state.result}</p>
+      </div>
+    )
+  }
+}
+
+export default TestScanner;
